1e0a8aea41ae76728261acc537424e7060b04998
tayor56 <adenusiadetayo@gmail.com>
2020-10-16 22:56:38 -0700

    uploading files to github

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+    //===== first major task. ======
+
+        /*
+            occupation:Student
+            Gender: male
+            Sidehustle Username: Tayor
+        */
+
+    //about me tag    
+    echo "<h1>About Me</h1> <hr/>";
+    
+    //your side hustle intenship username
+    echo "<h3>Tayor</h3>";
+
+    //tell me about yourself
+    echo "<p>My name is Adenusi Adetayo Abraham I am from Ogunstate. 
+    I attend Olabisi Onabanjo University am in my final Year of Chemistry </p>";
+
+    
+
+?>
+
+ 
